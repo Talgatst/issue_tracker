a278f6b2df090df337e4e33c4fc8a9a418d9f19e
Talgat <JorobekovT@gmail.com>
2025-11-30 09:44:37 +0600

    сделал 1-й этап

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from webapp.models import Status, Type, Issue
 
-# Register your models here.
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    pass
+
